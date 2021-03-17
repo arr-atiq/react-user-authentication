bbf52cf5b435bc95118a633b53c728a4957fcf38
aar-atiq <aar.msdeveloper01@gmail.com>
2021-03-17 10:30:29 +0600

    learned user authentication in firebase

---
+++ src/firebase.config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAXADrtr7M1p0XgGXGFDfHfHZWL8uO678M",
+    authDomain: "fir-ema.firebaseapp.com",
+    projectId: "fir-ema",
+    storageBucket: "fir-ema.appspot.com",
+    messagingSenderId: "935412588481",
+    appId: "1:935412588481:web:79c0736d44a98557355940",
+    measurementId: "G-KQSY6Z60HT"
+  };
+
+  export default firebaseConfig;
